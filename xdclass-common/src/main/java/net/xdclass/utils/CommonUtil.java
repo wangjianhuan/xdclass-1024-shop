5b477332fdea446b4ee0500f2a652d58fec35a94
wangjianhuan <wjh@updeMacBook-Pro.local>
2021-07-13 21:25:59 +0800

    token解密

--- xdclass-common/src/main/java/net/xdclass/utils/CommonUtil.java
+++ xdclass-common/src/main/java/net/xdclass/utils/CommonUtil.java
@@ -1,6 +1,12 @@
 package net.xdclass.utils;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.security.MessageDigest;
@@ -13,6 +19,7 @@ import java.util.UUID;
  * @date 2021/7/11 下午9:08
  * @QQ 1151777592
  */
+@Slf4j
 public class CommonUtil {
 
 
@@ -99,26 +106,30 @@ public class CommonUtil {
 
     /**
      * 获取当前时间戳
+     *
      * @return
      */
-    public static long getCurrentTimestamp(){
+    public static long getCurrentTimestamp() {
         return System.currentTimeMillis();
     }
 
     /**
      * 生成UUID
+     *
      * @return
      */
     public static String generateUUID() {
-        return UUID.randomUUID().toString().replaceAll("-","").substring(0,32);
+        return UUID.randomUUID().toString().replaceAll("-", "").substring(0, 32);
     }
 
     /**
      * 生成指定长度随机字母和数字
+     *
      * @param length
      * @return
      */
     private static final String ALL_CHAR_NUM = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
     public static String getStringNumRandom(int length) {
         //生成随机数字和字母,
         Random random = new Random();
@@ -127,5 +138,25 @@ public class CommonUtil {
             saltString.append(ALL_CHAR_NUM.charAt(random.nextInt(ALL_CHAR_NUM.length())));
         }
         return saltString.toString();
+    }
+
+    /**
+     * 响应json数据给前端
+     *
+     * @param response
+     * @param obj
+     */
+    public static void sendJsonMessage(HttpServletResponse response, Object obj) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        response.setContentType("application/json; charset=utf-8");
+
+        try (PrintWriter writer = response.getWriter()) {
+            writer.print(objectMapper.writeValueAsString(obj));
+
+            response.flushBuffer();
+
+        } catch (IOException e) {
+            log.warn("响应json数据给前端异常:{}", e);
+        }
     }
 }
--- xdclass-user-service/src/main/java/net/xdclass/interceptor/LoginInterceptor.java
+++ xdclass-user-service/src/main/java/net/xdclass/interceptor/LoginInterceptor.java
@@ -1,9 +1,75 @@
 package net.xdclass.interceptor;
 
+import io.jsonwebtoken.Claims;
+import lombok.extern.slf4j.Slf4j;
+
+import net.xdclass.enums.BizCodeEnum;
+import net.xdclass.model.LoginUser;
+import net.xdclass.utils.CommonUtil;
+import net.xdclass.utils.JWTUtil;
+import net.xdclass.utils.JsonData;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * @author WJH
  * @date 2021/7/13 下午8:00
  * @QQ 1151777592
  */
-public class LoginInterceptor {
+@Slf4j
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String accesstoken = request.getHeader("token");
+
+        if (accesstoken == null) {
+            accesstoken = request.getParameter("token");
+        }
+        if (StringUtils.isNoneBlank(accesstoken)) {
+            //获取到token
+            Claims claims = JWTUtil.checkJWT(accesstoken);
+
+            if (claims == null) {
+                //未登录
+                CommonUtil.sendJsonMessage(response, JsonData.buildResult(BizCodeEnum.ACCOUNT_UNLOGIN));
+                return false;
+            }
+
+            long userId = Long.valueOf(claims.get("id").toString());
+            String headImg = (String)claims.get("head_img");
+            String name = (String)claims.get("name");
+            String mail = (String)claims.get("mail");
+
+
+            LoginUser loginUser = new LoginUser();
+            loginUser.setName(name);
+            loginUser.setHeadImg(headImg);
+            loginUser.setId(userId);
+            loginUser.setMail(mail);
+
+            //通过attribute传递用户信息
+            //request.setAttribute("loginUser",loginUser);
+
+            // TODO: 2021/7/13  通过threadLocal传递用户登录信息  
+
+            return true;
+        }
+
+        CommonUtil.sendJsonMessage(response, JsonData.buildResult(BizCodeEnum.ACCOUNT_UNLOGIN));
+        return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+    }
 }
