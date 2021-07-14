bffe97d8e10ac5511c28e9af9a121767f1df1470
wangjianhuan <wjh@updeMacBook-Pro.local>
2021-07-14 11:30:11 +0800

    新增收获地址bug修复

--- xdclass-user-service/src/main/java/net/xdclass/service/Impl/AddressServiceImpl.java
+++ xdclass-user-service/src/main/java/net/xdclass/service/Impl/AddressServiceImpl.java
@@ -48,18 +48,22 @@ public class AddressServiceImpl implements AddressService {
         addressDO.setUserId(loginUser.getId());
         BeanUtils.copyProperties(addressAddRequest, addressDO);
 
-        //是否是默认收货地址
+        //是否有默认收货地址
         if (addressDO.getDefaultStatus() == AddressStatusEnum.DEFAULT_STATUS.getStatus()) {
             //查找数据库是否有默认地址
-            AddressDO defaultAddressDO = addressMapper.selectOne(new QueryWrapper<AddressDO>().eq("user_id", loginUser.getId()).eq("default_status", AddressStatusEnum.DEFAULT_STATUS));
+            AddressDO defaultAddressDO = addressMapper.selectOne(new QueryWrapper<AddressDO>()
+                    .eq("user_id", loginUser.getId())
+                    .eq("default_status", AddressStatusEnum.DEFAULT_STATUS.getStatus()));
+
             if (defaultAddressDO != null) {
+                //修改为非默认收货地址
                 defaultAddressDO.setDefaultStatus(AddressStatusEnum.COMMON_STATUS.getStatus());
-                addressMapper.update(defaultAddressDO,new QueryWrapper<AddressDO>().eq("id",defaultAddressDO.getId()));
+                addressMapper.update(defaultAddressDO, new QueryWrapper<AddressDO>().eq("id", defaultAddressDO.getId()));
             }
         }
 
         int rows = addressMapper.insert(addressDO);
 
-        log.info("新增收货地址:rows = {},data = {}", rows, addressDO);
+        log.info("新增收货地址:rows={},data={}", rows, addressDO);
     }
 }
